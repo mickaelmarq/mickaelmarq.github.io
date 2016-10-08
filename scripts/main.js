f0b4af0021ee044879e5c4f110a362535bd70cd8
Mickael Marquez <marquez.mickael@gmail.com>
2016-10-08 17:07:25 -0400

    new version

--- scripts/main.js
+++ scripts/main.js
@@ -9839,6 +9839,54 @@ $(function() {
   console.log('Welcome to Yeogurt!');
 });
 
+if(typeof InstallTrigger !== 'undefined'){
+  $('.work__img-container').scroll(function() {
+    var offset = -($(this).scrollLeft());
+    var asside = $(this).closest('.work').find('.two-face__scroll');
+    asside.css('left', (offset + 20)+'px');
+    // if(offset >= 100){
+    //   $(this).closest('.two-face__content').addClass('max');
+    // }else{
+    //   $(this).closest('.two-face__content').removeClass('max');
+    // }
+  });
+}else{
+  $('.work__img-container').scroll(function() {
+    var offset = -($(this).scrollLeft() - $(this).prop("scrollWidth") + $(this).innerWidth());
+    var asside = $(this).closest('.work').find('.two-face__scroll');
+    console.log($(this).scrollLeft());
+    asside.css('left', (offset + 20)+'px');
+    // if(offset >= 100){
+    //   $(this).closest('.two-face__content').addClass('max');
+    // }else{
+    //   $(this).closest('.two-face__content').removeClass('max');
+    // }
+  });
+}
+
+$(window).scroll(function() {
+  $('.two-face__content').removeClass('max');
+});
+
+// var curDown = false,
+//     curXPos = 0;
+// $('.work__img-container').mousemove(function(m){
+//   if(curDown === true){
+//    $(this).scrollLeft($(this).scrollLeft() + (curXPos - m.pageX)/4);
+//    console.log($(this).scrollLeft());
+//    console.log((curXPos - m.pageX));
+//   }
+// });
+//
+// $('.work__img-container').mousedown(function(m){
+//   curDown = true;
+//   curXPos = m.pageX;
+// });
+//
+// $('.work__img-container').mouseup(function(){
+//   curDown = false;
+// });
+
 },{"../_modules/link/link":2,"jquery":1}]},{},[3])
 
 
